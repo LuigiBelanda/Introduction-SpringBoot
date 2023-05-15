e0d1eed639526000d58b4e6b84a9d9060d34120f
Luigi Belanda <luigi.belanda.milani@gmail.com>
2023-05-15 16:07:50 -0300

    Test init projetc from command line

---
+++ 01-spring-boot-overview/05-command-line-demo/src/main/java/com/springapp/myappcoursespring/MyappcoursespringApplication.java
@@ -0,0 +1,13 @@
+package com.springapp.myappcoursespring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyappcoursespringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyappcoursespringApplication.class, args);
+	}
+
+}
