84afac46d156c2502e4fe3842dbae99d1edb55de
Luigi Belanda <luigi.belanda.milani@gmail.com>
2023-05-16 13:53:57 -0300

    Dependency Injection / @Autowired / @Component

---
+++ 02-spring-boot-spring-core/01-constructor-injection/src/main/java/com/compassuol/springcoredemo/Coach.java
@@ -0,0 +1,11 @@
+package com.compassuol.springcoredemo;
+
+// Criando nossa interface com um método
+// Em que nesse caso, toda classe que usar nossa interface (implements)
+// Terá que implementar nossa função abaixo
+public interface Coach {
+
+    // Função que deverá ser implementada pela classe que usar o implements da nossa interface
+    String getDailyWorkout();
+
+}
