1a17bcceaef4800b39fa4ad03f59c6ac229d326e
Luigi Belanda <luigi.belanda.milani@gmail.com>
2023-05-16 15:14:03 -0300

    Learing @Qualifier

---
+++ 02-spring-boot-spring-core/04-qualifiers/src/main/java/com/compassuol/springcoredemo/rest/DemoController.java
@@ -0,0 +1,26 @@
+package com.compassuol.springcoredemo.rest;
+
+import com.compassuol.springcoredemo.common.Coach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+    // Define a private field for the dependency
+    private Coach myCoach;
+
+    // Com a annotation abaixo (@Qualifier) deixamos claro qual Sptring Bean
+    // Queremos usar, basicamente colocamos o nome da classe que criamos, mas com
+    // A primeira letra em lower case, assim o nosso código irá direto no que queremos
+    @Autowired
+    public void DemoController(@Qualifier("baseballCoach") Coach theCoach) {
+        myCoach = theCoach;
+    }
+
+    @GetMapping("/dailyworkout")
+    public String getDailyWorkout() {
+        return myCoach.getDailyWorkout();
+    }
+}
---
+++ 02-spring-boot-spring-core/04-qualifiers/src/test/java/com/compassuol/springcoredemo/SpringcoredemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.compassuol.springcoredemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcoredemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
