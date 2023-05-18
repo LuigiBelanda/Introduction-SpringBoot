b9372dd87c98e205de46a7684192620aa4317452
Luigi Belanda <luigi.belanda.milani@gmail.com>
2023-05-18 15:12:43 -0300

    Add route to update Employee

--- 04-spring-rest-crud/02-spring-boot-rest-crud-employee/src/main/java/com/compassuol/cruddemo/rest/EmployeeRestController.java
+++ 04-spring-rest-crud/02-spring-boot-rest-crud-employee/src/main/java/com/compassuol/cruddemo/rest/EmployeeRestController.java
@@ -42,4 +42,10 @@ public class EmployeeRestController {
         return dbEmployee;
     }
 
+    @PutMapping("/employees")
+    public Employee updateEmployee(@RequestBody Employee theEmployee) {
+        Employee dbEmployee = employeeService.save(theEmployee);
+
+        return dbEmployee;
+    }
 }
