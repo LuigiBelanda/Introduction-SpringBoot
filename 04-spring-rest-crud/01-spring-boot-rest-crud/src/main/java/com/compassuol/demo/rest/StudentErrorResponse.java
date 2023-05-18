113143859c8c5d901a3e99e09437f7f1cdc5a0a7
Luigi Belanda <luigi.belanda.milani@gmail.com>
2023-05-18 11:18:49 -0300

    Create a custom error response class - Create a custom exception class

---
+++ 04-spring-rest-crud/01-spring-boot-rest-crud/src/main/java/com/compassuol/demo/rest/StudentErrorResponse.java
@@ -0,0 +1,41 @@
+package com.compassuol.demo.rest;
+
+public class StudentErrorResponse {
+    // Atributos do nosso erro caso não encontremos o usuário
+    private int status;
+    private String message;
+    private long timeStamp;
+
+    public StudentErrorResponse() {
+    }
+
+    public StudentErrorResponse(int status, String message, long timeStamp) {
+        this.status = status;
+        this.message = message;
+        this.timeStamp = timeStamp;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public long getTimeStamp() {
+        return timeStamp;
+    }
+
+    public void setTimeStamp(long timeStamp) {
+        this.timeStamp = timeStamp;
+    }
+}
