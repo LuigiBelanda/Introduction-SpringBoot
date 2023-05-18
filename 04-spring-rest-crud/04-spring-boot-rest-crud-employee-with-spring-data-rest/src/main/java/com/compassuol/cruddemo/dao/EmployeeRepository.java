94d704afeb277050606ae4f4fc795e568f383a73
Luigi Belanda <luigi.belanda.milani@gmail.com>
2023-05-18 16:14:25 -0300

    More configs

--- 04-spring-rest-crud/04-spring-boot-rest-crud-employee-with-spring-data-rest/src/main/java/com/compassuol/cruddemo/dao/EmployeeRepository.java
+++ 04-spring-rest-crud/04-spring-boot-rest-crud-employee-with-spring-data-rest/src/main/java/com/compassuol/cruddemo/dao/EmployeeRepository.java
@@ -2,10 +2,16 @@ package com.compassuol.cruddemo.dao;
 
 import com.compassuol.cruddemo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 // EmployeeRepository - Repository
 // Employee - Type Entity
 // Integer - PK
+
+// Com o @RepositoryRestResource podemos definir um novo nome para nosso endpoint
+// Em vez dele usar como base nossa Entity Name
+// Então nosso endpoint padrão agora é: /members
+// @RepositoryRestResource(path="members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
 }
